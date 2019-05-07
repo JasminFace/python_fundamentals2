69e4a57647f1fc68c386f7432b925b3d38b0f184
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-07 15:57:08 -0400

    completed exercise 2

---
+++ functions_ex2.py
@@ -0,0 +1,10 @@
+def negative(number):
+    if number < 0:
+        return True
+    else:
+        return False
+
+print(negative(-8))
+print(negative(64))
+print(negative(22))
+print(negative(-101))
